91a32141bbbe76d294e70059c9a563c70abefb05
WatchAndyTW <51250139+WatchAndyTW@users.noreply.github.com>
2019-09-14 21:58:16 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 mkdir -p /var/www/pterodactyl/vendor/pterodactyl/mobile-addon && cd /var/www/pterodactyl/vendor/pterodactyl/mobile-addon && git clone git@gitlab.com:redeyegrp/pterodactyl/mobile-addon.git .
