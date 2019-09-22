fce8a9ec8377fec8d83ebbdf0f36f90efaa1517b
WatchAndyTW <51250139+WatchAndyTW@users.noreply.github.com>
2019-09-22 19:55:31 +0800

    Update login.blade.php

--- resources/themes/pterodactyl/auth/login.blade.php
+++ resources/themes/pterodactyl/auth/login.blade.php
@@ -69,7 +69,7 @@
 					
 					<div class="container-login100-form-btn">
 						{!! csrf_field() !!}
-						<button type="submit" class="login100-form-btn btn btn-primary btn-block g-recaptcha pterodactyl-login-button--main" @if(config('recaptcha.enabled')) data-sitekey="{{ config('recaptcha.website_key') }}" data-callback='onSubmit' @endif>沒有帳號? 立即加入我們的DC群申請吧!</button>
+						<button type="submit" class="login100-form-btn btn btn-primary btn-block g-recaptcha pterodactyl-login-button--main" @if(config('recaptcha.enabled')) data-sitekey="{{ config('recaptcha.website_key') }}" data-callback='onSubmit' @endif>沒有帳號? 加入我們的DC申請吧!</button>
 					</div>
 
 					<div class="text-center p-t-12">
